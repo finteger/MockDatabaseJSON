f376f843d8428d0c0a21342fff6fdea8cbbd936e
finteger <104397474+finteger@users.noreply.github.com>
2023-11-08 11:43:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 
 const app = express();
 const port = 3000;
-<<<<<<< HEAD
 
 const filePath = "./database.json";
 
@@ -17,26 +16,21 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("home.ejs");
 });
-=======
->>>>>>> 47accfd1ff457d2c3f7a739088acdda6e1edb6ea
 
 app.get("/users/:id", (req, res) =>{
     const data = JSON.parse(fs.readFileSync(filePath));
     const user = data.users.find((user) => user.id === parseInt(req.params.id));
     const userData = JSON.stringify(user);
 
-<<<<<<< HEAD
    res.render('home.ejs', {userData});
-=======
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
->>>>>>> 47accfd1ff457d2c3f7a739088acdda6e1edb6ea
 });
 
 app.listen(port, () => {
     console.log(`Connected on port ${port}`);
 });
+
+
+
 
 
 
