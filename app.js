47accfd1ff457d2c3f7a739088acdda6e1edb6ea
finteger <104397474+finteger@users.noreply.github.com>
2023-11-08 09:13:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 const express = require("express");
 
 const app = express();
-const port = 8080;
-
-//Middleware
-
-function executeMiddleware(req, res, next){
-    console.log("This is middleware!");
-    next();
-}
-
-app.get("/", executeMiddleware, (req, res) => {
-    console.log("Hello World!");
-});
+const port = 3000;
 
 
-app.get('/products/:id', (req, res,) => {
-    const productId = req.params.id;
-    res.send(`Product with ID ${productId}`);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
 
@@ -25,4 +14,6 @@ app.get('/products/:id', (req, res,) => {
 app.listen(port, () => {
     console.log(`Connected on port ${port}`);
 })
+
+
 
