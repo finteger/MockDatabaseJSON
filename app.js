29f739b2065d18d988093a7e0d5970b0320da80e
finteger <nashassociatesinc@gmail.com>
2023-11-08 11:25:56 -0700

    My Second Commit

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 const express = require("express");
+const ejs = require("ejs");
+const fs = require("fs");
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
-//Middleware
+const filePath = "./database.json";
 
-function executeMiddleware(req, res, next){
-    console.log("This is middleware!");
-    next();
-}
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true})); //for parsing application/x-www-form-urlencoded
 
-app.get("/", executeMiddleware, (req, res) => {
-    console.log("Hello World!");
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
 });
 
+app.get("/users/:id", (req, res) =>{
+    const data = JSON.parse(fs.readFileSync(filePath));
+    const user = data.users.find((user) => user.id === parseInt(req.params.id));
+    const userData = JSON.stringify(user);
 
-app.get('/products/:id', (req, res,) => {
-    const productId = req.params.id;
-    res.send(`Product with ID ${productId}`);
+   res.render('home.ejs', {userData});
 });
-
-
 
 app.listen(port, () => {
     console.log(`Connected on port ${port}`);
-})
+});
 
