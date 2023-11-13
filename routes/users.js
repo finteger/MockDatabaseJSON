8ba6f6c2b2f4da2c8eec4f7d2d986bb4dd446465
finteger <104397474+finteger@users.noreply.github.com>
2023-11-13 11:37:48 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs").promises;
 
+const filePath = "./database.json"; //Link to our mock database
+
+router.use(express.json());  // parse JSON
+router.use(express.urlencoded({ extended: true})); //handle form data
+
+
+async function readData(){
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
+}
+
+async function writeData(data){
+     await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+}
+
+router.get("/", async (req, res) => {
+
+});
+
+
+
+module.exports = router;
